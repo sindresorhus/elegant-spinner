80a644e32c44d6176e06dbc4f2e42206f930d129
Noam Okman <noamokman@gmail.com>
2015-10-02 12:11:02 +0300

    one line as suggested

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ module.exports = function () {
 	var i = 0;
 
 	return function () {
-		var frame = frames[i];
-
-		i = (i + 1) % frames.length;
-
-		return frame;
+		return frames[i = ++i % frames.length];
 	};
 };
 
