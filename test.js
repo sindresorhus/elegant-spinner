976dbd08537de5c511a5a5a743bfd6a28410e70a
flipactual <flipstewart@me.com>
2016-01-30 15:48:09 -0600

    add bounce style

--- test.js
+++ test.js
@@ -2,8 +2,7 @@ import test from 'ava';
 import m from './';
 
 test(t => {
-	t.true(Array.isArray(m.frames));
-	const frame = m();
+	const frame = m({style: 'spinner'});
 	t.not(frame(), frame());
 	t.not(frame(), frame());
 	t.not(frame(), frame());
